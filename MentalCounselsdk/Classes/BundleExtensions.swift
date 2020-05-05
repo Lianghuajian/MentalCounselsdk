51116e6bfd810aabbf12acfb3c9829f28a09e61c
lianghuajian <624935495@qq.com>
2020-05-05 22:57:39 +0800

    bundle兼容framework

--- MentalCounselsdk/Classes/BundleExtensions.swift
+++ MentalCounselsdk/Classes/BundleExtensions.swift
@@ -9,6 +9,7 @@ import Foundation
 
 public extension Bundle {
     convenience init?(name: String) {
+        Bundle.main.path(forResource: name, ofType: "bundle")
         guard let resourcePath = Bundle.main.resourcePath else {
             self.init()
             return
@@ -18,6 +19,17 @@ public extension Bundle {
             bundleURL.appendPathExtension("bundle")
         }
         if !FileManager.default.fileExists(atPath: bundleURL.path)  {
+            if var frameworkURL = Bundle.main.privateFrameworksURL {
+                //load bundle from framework
+                frameworkURL.appendPathComponent("\(name).framework")
+                frameworkURL.appendPathComponent("\(name).bundle")
+                if !FileManager.default.fileExists(atPath: frameworkURL.path) {
+                    self.init()
+                    return
+                }
+                self.init(path: frameworkURL.path)
+                return
+            }
             assert(false, "Bundle not found in: \(bundleURL.path)")
             self.init()
             return
@@ -25,4 +37,5 @@ public extension Bundle {
         self.init(path: bundleURL.path)
     }
 }
+
 
